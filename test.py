2e10366935c547aa98497bd692f312a735fcbc01
0xJacky <71195766+0xJacky-LZK@users.noreply.github.com>
2020-09-13 10:19:45 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 number = int(input("Please enter the number :"))
 if( number % 2 == 0 ):
-    print("This is a even number.")
+    print("The number is %d, This is a even number." % number)
 else:
     print("This is a odd number.")
